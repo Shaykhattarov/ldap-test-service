d34b0a983ba2864402a8dd61f0b60583722a48d1
Шайхаттаров Ильдан Ильдарович <Shayhattarov@ibs.root.ibscorp.ru>
2025-09-17 12:11:29 +0300

    fix - Небольшие исправления

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ def main():
     
     config = LDAPConfig(
         host=settings.ldap_server_host,
-        port=int(settings.ldap_server_port),
+        port=settings.ldap_server_port,
         bind_dn=settings.ldap_app_dn,
         bind_password=settings.ldap_app_password,
         use_ssl=settings.ldap_use_tls,
